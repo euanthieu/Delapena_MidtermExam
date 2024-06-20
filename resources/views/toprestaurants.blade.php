8b905e9412bf0d6655be776d43ae511973233520
euanthieu <euanthieu@gmail.com>
2024-06-20 18:46:17 +0800

    Update: View added: toprestaurants.blade.php

---
+++ resources/views/toprestaurants.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Top Restaurants in Baguio City</title>
+</head>
+<body>
+    <h1>Top Restaurants in Baguio City</h1>
+    <p>1: {{ $r1 }}</p>
+    <p>2: {{ $r2 }}</p>
+    <p>3: {{ $r3 }}</p>
+    <p>4: {{ $r4 }}</p>
+    <p>5: {{ $r5 }}</p>
+    <nav>
+</body>
+</html>
